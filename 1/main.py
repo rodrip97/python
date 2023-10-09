99c8380877ebedd2ce49514da00f429bba1db5b3
rodrip97 <zimmer672@gmail.com>
2023-10-09 17:09:16 -0400

    practice practice practice

---
+++ 1/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+#lists and data types
+my_list = ['apple', 'oranges', 2, 4,3 ]
+
+print(len(my_list))
+
+my_list.pop(3)
+print(my_list)
+
+userAge = [21, 23 ,25, 27]
---
+++ exercises/calculator.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+#the goal of this exercise is to create a calculator. Nothing fancy but it needs to work well
+
+def sum(a, b):
+    return a+b
+
+def subtract (a,b):
+    return a-b
