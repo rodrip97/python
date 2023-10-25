1b6087263feb010e7f301ce09ffe219b64c49e7a
rodrip97 <zimmer672@gmail.com>
2023-10-25 16:32:28 -0400

    forgot which day

--- exercises/calculator.py
+++ exercises/calculator.py
@@ -11,11 +11,19 @@ def divide(n1,n2):
     return n1 / n2
 
 
-operatrions = {
+operations = {
     '+' : add,
     '-' : sub,
     '*' : multiply,
     '/' : divide,
 }
 
-num1 = input("What's the first number?:")
+num1 = float(input("What's the first number?: "))
+operation = input('Please select the operation you would like to perform: + , - , / , *: ')
+num2 = float(input("What's the second number?: "))
+
+if operation in operations:
+    result = operations[operation](num1, num2)
+    print('Your result is:', result)
+else:
+    print('Invalid operation. Please enter +, -, *, or /')
---
+++ pandas/dfs.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\RodrigoPinto\\Desktop\\100\\pandas\\game_logs.csv')
+
+print(df.info)
