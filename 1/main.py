462fbc45eab6b9584185a6f63cd6c04673e58eea
rodrip97 <zimmer672@gmail.com>
2023-12-11 16:02:40 -0500

    working up

--- 1/main.py
+++ 1/main.py
@@ -1,12 +1,41 @@
-import pandas as pd
+# import pandas as pd
+#
+#
+# #lists and data types
+# my_list = ['apple', 'oranges', 2, 4,3 ]
+#
+# print(len(my_list))
+#
+# my_list.pop(3)
+# print(my_list)
+#
+# userAge = [21, 23 ,25, 27]
 
 
-#lists and data types
-my_list = ['apple', 'oranges', 2, 4,3 ]
+# def get_bmi(height, weight):
+#     return weight / (height * height)
+#
+# height_input = input("Enter your height: ")
+# weight_input = input("Enter your weight: ")
+#
+# bmi = get_bmi(float(height_input), float(weight_input))
+# print(f'your BMI is: {bmi}')
+#
+#
+## Do the tip calculator here
+#
+# bill_total = input("What is the total for the bill? ")
+# tip_percentage = input("What percentage would you like to tip? ")
+# people_count = input("How many people are splitting the bill? ")
+# total_with_tip = (int(bill_total) + int(bill_total) *  int(tip_percentage) / 100) / int(people_count)
+#
+# print(f"Your total is: {bill_total}, if you want to tip {tip_percentage}%, then total will be: {total_with_tip}$, for {people_count} people.")
 
-print(len(my_list))
+print("Add a number you want to add! for example 12 is 13!")
 
-my_list.pop(3)
-print(my_list)
+numbers = input("Input number now ....")
 
-userAge = [21, 23 ,25, 27]
+sum_numbers = int(numbers[0]) + int(numbers[1])
+print(f"Your number is: {sum_numbers}")
+
+print(type(sum_numbers))
--- exercises/hangman.py
+++ exercises/hangman.py
@@ -1,31 +1,46 @@
 import random
 
-
 word_list = ['python', 'hangman', 'programming', 'computer', 'challenge', 'knowledge', 'development', 'learning']
 secret_word = random.choice(word_list)
 guesses = ''
 turns = 10
 
+while True:
+    while turns > 0:
+        missed = 0
+        for letter in secret_word:
+            if letter in guesses:
+                print(letter, end='')
+            else:
+                print('-', end='')
+                missed += 1
 
-while turns > 0 :
-    missed = 0
-    for letter in secret_word:
-        if letter in guesses:
-            print(letter, end='')
-        else: 
-            print('-', end='')
-            missed += 1
-    
-    if missed == 0:
-        print('\n Congrats! You Won!')
-        break
-    
-    guess = input('\n Guess a letter:')
-    guesses += guess
+        if missed == 0:
+            print('\n Congrats! You Won!')
+            ans = input("Play again Y/N?")
+            if ans.upper() == 'Y':
+                secret_word = random.choice(word_list)
+                guesses = ''
+                turns = 10
+                break
+            else:
+                exit()
 
-    if guess not in secret_word:
-        turns -= 1
-        print('Try again!')
-    if turns == 0:
-        print('Game over, your word was:', secret_word)
+        guess = input('\n Guess a letter:')
+        guesses += guess
 
+        if guess not in secret_word:
+            turns -= 1
+            print('Try again!')
+            print('You have', turns, 'more guesses')
+
+        if turns == 0:
+            print('Game over, your word was:', secret_word)
+            ans = input("Play again Y/N?")
+            if ans.upper() == 'Y':
+                secret_word = random.choice(word_list)
+                guesses = ''
+                turns = 10
+                break
+            else:
+                exit()
